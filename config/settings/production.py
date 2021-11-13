aab8e86425851d879736f1e212488975f57d1731
buckldav <runalldayandnight@gmail.com>
2021-11-12 22:32:37 -0700

    Redis

--- config/settings/production.py
+++ config/settings/production.py
@@ -20,7 +20,7 @@ DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # no
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": env("REDIS_URL"),
+        "LOCATION": env("REDISTOGO_URL"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             # Mimicing memcache behavior.
