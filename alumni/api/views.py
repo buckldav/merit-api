56665f100b1ed7267cba5f9f978281be9e6f32dd
David Buckley <runalldayandnight@gmail.com>
2022-04-19 08:21:59 -0600

    API Permissions Alumni

--- alumni/api/views.py
+++ alumni/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.permissions import AllowAny
 
 from alumni.models import Alum
 from alumni.api.serializers import AlumSerializer
@@ -7,3 +8,4 @@ from alumni.api.serializers import AlumSerializer
 class AlumViewSet(ReadOnlyModelViewSet):
     serializer_class = AlumSerializer
     queryset = Alum.objects.all()
+    permission_classes = [AllowAny]
